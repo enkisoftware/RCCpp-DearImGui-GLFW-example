a3b8c2ca19fc13805cfbe33a63413ce27bed892c
juliettef <juliette@enkisoftware.com>
2020-02-06 15:49:11 +0000

    Added logger and RCC++ code to main

--- main.cpp
+++ main.cpp
@@ -6,11 +6,27 @@
 
 #include <GLFW/glfw3.h>
 
+#include "RuntimeObjectSystem.h"
+
+// headers from our example 
+#include "StdioLogSystem.h"
+
+// RCC++ Data
+static IRuntimeObjectSystem*	g_pRuntimeObjectSystem;
+static StdioLogSystem           g_Logger;
+
+bool RCCppInit();
+void RCCppCleanup();
+void RCCppUpdate();
 
 int main( int argc, const char * argv[] )
 {
     if (!glfwInit())
         exit(1);
+
+    // Initialize RCC++
+    RCCppInit();
+
     GLFWwindow* window = glfwCreateWindow(1280, 720, "CMake RCC++ Dear ImGui starter", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
@@ -35,6 +51,9 @@ int main( int argc, const char * argv[] )
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // Update RCC++
+        RCCppUpdate();
+
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         static bool show_demo_window = true;
         if (show_demo_window)
@@ -52,10 +71,43 @@ int main( int argc, const char * argv[] )
     }
 
     // Cleanup
+    RCCppCleanup();
     ImGui_ImplOpenGL2_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
     glfwTerminate();
 
     return 0;
+}
+
+bool RCCppInit()
+{
+    g_pRuntimeObjectSystem = new RuntimeObjectSystem;
+	if( !g_pRuntimeObjectSystem->Initialise(&g_Logger, NULL) )
+    {
+        g_pRuntimeObjectSystem = NULL;
+        return false;
+    }
+    return true;
+}
+
+void RCCppCleanup()
+{
+	delete g_pRuntimeObjectSystem;
+}
+
+void RCCppUpdate()
+{
+    //check status of any compile
+    if( g_pRuntimeObjectSystem->GetIsCompiledComplete() )
+    {
+        // load module when compile complete
+        g_pRuntimeObjectSystem->LoadCompiledModule();
+    }
+
+    if( !g_pRuntimeObjectSystem->GetIsCompiling() )
+    {
+        float deltaTime = 1.0f / ImGui::GetIO().Framerate;
+        g_pRuntimeObjectSystem->GetFileChangeNotifier()->Update( deltaTime );
+    }
 }
